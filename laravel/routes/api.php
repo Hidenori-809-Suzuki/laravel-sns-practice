f0121427f2820c6e546cadc6150ed21b7ff5c630
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2021-12-29 04:07:19 +0900

    2-4Finished

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//使わないのでコメントアウト 2021.12.29
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
