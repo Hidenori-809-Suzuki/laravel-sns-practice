5380044fe7995fe5ce89beb80b9e91b549e49105
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2022-01-09 03:04:38 +0900

    section8-6-finished

--- laravel/app/Http/Requests/ArticleRequest.php
+++ laravel/app/Http/Requests/ArticleRequest.php
@@ -28,6 +28,7 @@ class ArticleRequest extends FormRequest
             'title' => 'required|max:50',
             'body' => 'required|max:500',
             //==========ここまで追加==========
+            'tags' => 'json|regex:/^(?!.*\s).+$/u|regex:/^(?!.*\/).*$/u',
         ];
     }
 
@@ -37,7 +38,16 @@ class ArticleRequest extends FormRequest
         return [
             'title' => 'タイトル',
             'body' => '本文',
+            'tags' => 'タグ',
         ];
     }
     //==========ここまで追加==========
+    public function passedValidation()
+    {
+        $this->tags = collect(json_decode($this->tags))
+            ->slice(0, 5)
+            ->map(function ($requestTag) {
+                return $requestTag->text;
+            });
+    }
 }
